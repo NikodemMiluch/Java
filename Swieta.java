b48a61a0632c33a76ff292ec6eb9da8295efac4c
Nikodem Miłuch <58949250+NikodemMiluch@users.noreply.github.com>
2019-12-16 20:18:52 +0100

    Add files via upload

---
+++ Swieta.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Swieta
+{
+    public static void main(String[] args) 
+    {
+       /* String a;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Chcesz zobaczyć kartkę świąteczną: ");
+        a = scanner.next();
+        
+        if(result=="tak")*/
+        {
+        System.out.println("╔═════════════════════════════════════════════════════════════════╗");
+        System.out.println("║	_[_]_	   ┌─────────────────────────┐      _[_]_	  ║");
+        System.out.println("║       (**)       │  Wszystkiego Najlepszego|	     (**)         ║");
+        System.out.println("║   `--( : )--'    │	Z okazji	     │    `--( : )--'    ║");
+        System.out.println("║     (  :  )      │ Świąt Bożego Narodzienia│      (  :  ) 	  ║");
+        System.out.println("║   		   └─────────────────────────┘       		  ║");
+        System.out.println("╚═════════════════════════════════════════════════════════════════╝");
+        }
+     /*   else if (result=="nie")
+        {
+            System.out.println("to nie");
+        }
+        */
+    }
+
+}
