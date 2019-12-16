a1330d5fe782c399dcf69edaf2bf92d0c6b0f793
NikodemMiluch <58949250+NikodemMiluch@users.noreply.github.com>
2019-12-16 17:29:21 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.sca;
+
+class Main {
+  public static void main(String[] args) {
+
+
+    int a;
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Podaj liczbę:");
+    a = scanner.nextInt();
+    if(a%2==0)
+    {
+      System.out.println("Liczba jest parzysta");
+    }
+    else 
+    System.out.println("Liczba nie jest parzysta");
+    }
+}
