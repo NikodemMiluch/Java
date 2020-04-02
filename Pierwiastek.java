d38ef23e7d219f819457a6cb804e7c9ceefa4194
Nikodem Miłuch <nikodemus1603@gmail.com>
2020-04-02 13:48:30 +0200

    02.04.2020
    -zmiany

--- Pierwiastek.java
+++ Pierwiastek.java
@@ -4,8 +4,7 @@ class Pierwiastek {
     // Program który, jezeli pierwiastek z liczby jest calkowity wypisuje go, jezeli nie, daje pomiedy jakimi liczbmi znajduje sie ta liczba
     System.out.println("Podaj liczbę: ");
     Scanner sc = new Scanner(System.in);
-    int n;
-    n = sc.nextInt();
+    int n = sc.nextInt();
     int i = 2;
     for(i = 2; i * i <= n; i++ ){}
     System.out.println(i-1);
