aebefe8c8a2ad06dfbabf066eca75cb1b602f07c
Nikodem Miłuch <nikodemus1603@gmail.com>
2020-06-03 11:41:58 +0200

    03.06

--- Tamagotchi.java
+++ Tamagotchi.java
@@ -1,7 +1,8 @@
 import java.util.*; // co można tu jescze dodać?
-class Main
+import java.io.*;
+class Tamagotchi
 {
-    public static void main(String args[])
+    public static void main(String args[]) throws FileNotFoundException 
     {
         Scanner sc = new Scanner(System.in);
         int a = 1; //jedzenie +fast food ++zdrowe
@@ -9,6 +10,7 @@ class Main
         int c = 1; //zdrowotnosc +ćwiczenia, ++leki
         int d = 1; //zmęczenie
         int p = 50; // j(10), dj(20); lek(20)
+        PrintWriter w = new PrintWriter("Wynik.txt");
         System.out.println("Cześć, jestem Tamagotchi! Musisz zadbać za moje zapotrzebowania! Po każdym dniu dostajesz 20 zł!");
         for(int i = 0;i<20;i++)
         {
@@ -20,7 +22,7 @@ class Main
                 a -= 1;
                 b += 1;
             }
-            if(x==2)
+            else if(x==2)
             {
                 System.out.println("Jakie kupimy jedzienie, fast food(1) za 20 czy sałatkę(2) za 10");
                 int x2 = sc.nextInt();
@@ -43,7 +45,7 @@ class Main
                     p -= 10;
                 }
             }
-            if(x==3)
+            else if(x==3)
             {
                 System.out.println("Musze wziąśc leki(1), czy ćwiczyć(2)?");
                 int x3 = sc.nextInt();
@@ -73,20 +75,48 @@ class Main
                         d -= 1;
                     }
                 }
-
-                //Podsumowanie dnia
-                System.out.print("Tamogotchi jest: ");
-                if(a >= 1){System.out.print("Najedzony ");}
-                if(a < 1){System.out.print("Głodny ");}
-                if(b >= 1){System.out.print("Szczęśliwy ");}
-                if(b < 1){System.out.print("Smutny ");}
-                if(c >= 1){System.out.print("Zdrowy ");}
-                if(c < 1){System.out.print("Chory ");}
-                if(d >= 1){System.out.print("Zenergizowany ");}
-                if(d < 1){System.out.print("Zmęczony ");}
-                System.out.println();
-                p += 20;
             }
+             //Podsumowanie dnia
+             System.out.print("Tamogotchi jest: ");
+             if(a >= 1){System.out.print("Najedzony ");}
+             if(a < 1){System.out.print("Głodny ");}
+             if(b >= 1){System.out.print("Szczęśliwy ");}
+             if(b < 1){System.out.print("Smutny ");}
+             if(c >= 1){System.out.print("Zdrowy ");}
+             if(c < 1){System.out.print("Chory ");}
+             if(d >= 1){System.out.print("Zenergizowany ");}
+             if(d < 1){System.out.print("Zmęczony ");}
+             System.out.println();
+             p += 20;
+        }
+        System.out.println("Czas wakacji Tamagotchi się skończył! Podsumowanie twojej gry");
+        System.out.println("Statystyka głodu: " + a);
+        System.out.println("Statystyka szczęścia: " + b);
+        System.out.println("Statystyka zdrowia: " + c);
+        System.out.println("Statystyka zmęczenia: " + d);
+        System.out.println("Statystyka pieniędzy: " + p);
+        System.out.println("Czy chcesz aby twoje wyniki zostały zapisane w tabeli wyników? Tak(1), Nie(2)");
+        String e = sc.next();
+        if(e.equals("1"))
+        {
+            System.out.println("Zobacz sobie wyniki poprzenich graczy:");
+            
+            System.out.println("Podaj swój nick");
+            e = sc.nextLine();
+            w.println("Nick: " + e);
+            w.println("a= " + a);
+            w.println("b= " + b);
+            w.println("c= " + c);
+            w.println("d= " + d);
+            w.println("p= " + p);
+        }
+        else if(e.equals("0"))
+        {
+            System.out.println("Okej, mamy nadzieje że się podobała nasza gra!");
+        }
+        else
+        {
+            System.out.println("Nie możesz wpisać nic innego, niż 1 i 2 ale rozumiem to jako, że nie chcesz zostać zapisny");
         }
     }
 }
